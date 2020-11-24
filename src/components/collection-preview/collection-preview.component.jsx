d880df896e29e52fc3d990bc4563271241b58eee
toniemati <matikas2000@gmail.com>
2020-11-24 22:53:50 +0100

    added redux, implement reducer and actions to header and app

--- src/components/collection-preview/collection-preview.component.jsx
+++ src/components/collection-preview/collection-preview.component.jsx
@@ -6,7 +6,7 @@ import './collection-preview.styles.scss';
 
 const CollestionPreview = ({ title, items }) => (
     <div className='collection-preview'>
-        <h1 class='title'>{title.toUpperCase()}</h1>
+        <h1 className='title'>{title.toUpperCase()}</h1>
         <div className='preview'>
             {
                 items.filter((item, idx) => idx < 4)
